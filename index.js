3e207c162ecf66f37198241bff9482692cbde987
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:24:22 +0700

    TUGAS 22 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,10 @@
+function panggilForeach() {
+    var dataKata = ["Saya", "ingin", "belajar", "bersama"];
+    // dataKata.forEach(function (item, index) {
+    //     console.log("Item :", item, "Index ke ", index);
+    // })
+    dataKata.forEach((item, index) => {
+        console.log("Item :", item, "Index ke ", index);
+    })
+}
+panggilForeach();
